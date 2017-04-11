6930291254c1a7cb50575b53d3a044ff59b9480a
xuchao <85825770@qq.com>
2017-04-11 15:51:44 +0800

    init

---
+++ DatetimePicker.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\bootstrap\datetimepicker;
+
+use yii\widgets\InputWidget;
+
+/**
+ * Class DatetimePicker
+ * @package xutl\bootstrap\datetimepicker
+ */
+class DatetimePicker extends InputWidget
+{
+
+}
